a41c6d8ff2d9ed1151cfab75a98e5b25532f0c60
ihor-romaniuk <ihor.romaniuk@raccoongang.com>
2026-01-05 12:51:10 +0100

    fix: [NAU-591] correct gamma profile API url

--- dist/data/urls.js
+++ dist/data/urls.js
@@ -5,6 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.getGammaProfileUrl = void 0;
 var _frontendPlatform = require("@edx/frontend-platform");
-const getGammaProfileUrl = username => `${(0, _frontendPlatform.getConfig)().LMS_BASE_URL}/gamma_dashboard/api/v0/avatar-progress/${username}`;
+const getGammaProfileUrl = username => `${(0, _frontendPlatform.getConfig)().LMS_BASE_URL}/gamma_dashboard/api/v0/avatar-progress/${username}/`;
 exports.getGammaProfileUrl = getGammaProfileUrl;
 //# sourceMappingURL=urls.js.map
